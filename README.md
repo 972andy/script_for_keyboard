b72a60fbddf32356c919f15aacb6bdbcbfd1acac
Andrii Tsapko <sandriy972@gmail.com>
2024-08-15 16:32:00 +0300

    add changes, set delay to 0.001 ms

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # script_for_keyboard
+
+1 Встановіть бібліотеку pynput
+пропиши в терміналі з правами адміністратора
+pip install pynput
+
+2 щоб запустити скрипт пропиши в терміналі python3 key_listener.py
+python3 key_listener.py
--- key_listener.py
+++ key_listener.py
@@ -1,28 +1,56 @@
 from pynput import keyboard
+import threading
+
+a_pressed_once = False
+d_pressed_once = False
+action_completed = False  
+
+def reset_state():
+    global a_pressed_once, d_pressed_once, action_completed
+    a_pressed_once = False
+    d_pressed_once = False
+    action_completed = False  
+    print("Скидання стану через таймер. Можна знову натискати 'A' і 'D'.")
 
 def on_press(key):
+    global a_pressed_once, d_pressed_once, action_completed
+    if action_completed:  
+        return
+    
     try:
-        if key.char == 'a':
+        if key.char == 'a' and not a_pressed_once:
             print("Натиснута клавіша 'A'")
-        elif key.char == 'd':
+        elif key.char == 'd' and not d_pressed_once:
             print("Натиснута клавіша 'D'")
     except AttributeError:
         pass
 
 def on_release(key):
+    global a_pressed_once, d_pressed_once, action_completed
+    if action_completed:  
+        return
+    
     try:
-        if key.char == 'a':
+        if key.char == 'a' and not a_pressed_once:
             print("Відпущена клавіша 'A'")
             keyboard.Controller().press('d')
             keyboard.Controller().release('d')
             print("Натиснута клавіша 'D'")
-        elif key.char == 'd':
+            a_pressed_once = True  
+            action_completed = True  
+            threading.Timer(0.001, reset_state).start()  
+        elif key.char == 'd' and not d_pressed_once:
             print("Відпущена клавіша 'D'")
+            
             keyboard.Controller().press('a')
             keyboard.Controller().release('a')
             print("Натиснута клавіша 'A'")
+            d_pressed_once = True  
+            action_completed = True  
+            threading.Timer(0.001, reset_state).start()  
     except AttributeError:
         pass
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+    
